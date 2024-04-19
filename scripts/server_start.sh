7b39902f05fa2c068c54fd4a70982ead8e8cb7b1
tanya0131 <tanyakumari0131@gmail.com>
2024-04-19 16:25:28 +0530

    server_start change

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -3,8 +3,6 @@
 # Change directory to where your frontend application is located
 cd /home/ec2-user/fitnessapp/
 
-# Install dependencies
-npm install
-
-# Start the server
-npm start
+# Start the frontend application
+sudo npm install
+sudo npm start > /dev/null 2>&1 &
