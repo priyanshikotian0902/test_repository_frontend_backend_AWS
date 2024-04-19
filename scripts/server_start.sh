f0c81da08a9db6ba5c8d384817da38fce8f3f7ac
priyanshik0902 <latakotian09@gmail.com>
2024-04-19 19:22:25 +0530

    6 commit

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 # Change directory to where your frontend application is located
-<<<<<<< HEAD
 cd /home/ec2-user/server/fitnessapp/build
-=======
-cd /home/ec2-user/fitnessapp/
->>>>>>> 7b39902f05fa2c068c54fd4a70982ead8e8cb7b1
+
+
 
 # Start the frontend application
 sudo npm install
