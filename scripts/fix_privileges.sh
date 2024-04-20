aef9daf9652a279f479e6c41f9cdefc85943e736
priyanshik0902 <latakotian09@gmail.com>
2024-04-20 11:08:49 +0530

    2

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -2,3 +2,6 @@
 
 # Change directory to where your server scripts are located
 chown -R ec2-user:ec2-user /var/www/html
+chmod +x /home/ec2-user/server/*.jar
+chmod +x /home/ec2-user/server/server_start.sh
+chmod +x /home/ec2-user/server/server_stop.sh
--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+cd /home/ec2-user/server
+sudo java -jar *.jar > /dev/null 2> /dev/null < /dev/null &
 # Change directory to where your frontend application is located
 cd /var/www/html
 npm start
